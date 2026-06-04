0bfbeee611df67b088ce604cf505efe0f4f5a22a
Weiyu-Kong <1625827540@qq.com>
2026-06-04 15:17:22 +0000

    fix: export base module

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,4 +1,5 @@
+from app.db.session import Base
 from app.models.requirement import Requirement
 from app.models.submission import Submission
 
-__all__ = ["Requirement", "Submission"]
+__all__ = ["Base", "Requirement", "Submission"]
