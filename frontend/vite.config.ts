f14a947c17eb9a04bb7c2ba3dd8814b1b960c5bc
Weiyu-Kong <1625827540@qq.com>
2026-07-25 12:07:11 +0800

    feat: update proxy path for paper-assets in Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         target: "http://127.0.0.1:8000",
         changeOrigin: true,
       },
-      "/assets": {
+      "/paper-assets": {
         target: "http://127.0.0.1:8000",
         changeOrigin: true,
       },
